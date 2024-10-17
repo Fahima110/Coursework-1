f8532ccd6c1a8952fafd6a2b14188e24c338bf7d
Fahima110 <Fahima8du@gmail.com>
2024-10-17 11:52:29 +0400

    first commit

---
+++ lessons.js
@@ -0,0 +1,82 @@
+export const lessons = [
+    {
+        title: "Maths Lesson",
+        subject: "Maths",
+        location: "Room G-1-08",
+        price: 10,
+        availability: 5,
+        images: "Images/Math.png"
+    },
+    {
+        title: "English Lesson",
+        subject: "English",
+        location: "Room A-2-03",
+        price: 15,
+        availability: 5,
+        images: "Images/English.png"
+    },
+    {
+        title: "PE Lesson",
+        subject: "Physical Education",
+        location: "Sports Hall",
+        price: 20,
+        availability: 5,
+        images: "Images/PE.png"
+    },
+    {
+        title: "Art Class",
+        subject: "Arts",
+        location: "Art Studio",
+        price: 12,
+        availability: 5,
+        images: "Images/Art.png"
+    },
+    {
+        title: "Badminton Practice",
+        subject: "Badminton",
+        location: "Sports Hall",
+        price: 18,
+        availability: 5,
+        images: "images/Badminton.png"
+    },
+    {
+        title: "Music Class",
+        subject: "Music",
+        location: "Music Room",
+        price: 22,
+        availability: 5,
+        images: "images/Music.png"
+    },
+    {
+        title: "Robotics Workshop",
+        subject: "Robotics",
+        location: "Tech Lab",
+        price: 25,
+        availability: 5,
+        images: "images/Robotics.png"
+    },
+    {
+        title: "Law Class",
+        subject: "Law",
+        location: "Room B-1-12",
+        price: 30,
+        availability: 5,
+        images: "images/Law.png"
+    },
+    {
+        title: "Business Studies",
+        subject: "Business",
+        location: "Room C-2-06",
+        price: 28,
+        availability: 5,
+        images: "images/Business.png"
+    },
+    {
+        title: "Padel Training",
+        subject: "Padel",
+        location: "Tennis Courts",
+        price: 20,
+        availability: 5,
+        images: "images/Padel.png"
+    }
+];
